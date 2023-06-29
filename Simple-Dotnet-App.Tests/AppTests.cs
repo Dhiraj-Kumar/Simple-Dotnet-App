047899a22848cfc9b1f3139566378cb53ab6795e
Dhiraj.kumar <dhiraj.kumar@stackroute.in>
2023-06-30 01:07:08 +0530

    initial commit

---
+++ Simple-Dotnet-App.Tests/AppTests.cs
@@ -0,0 +1,11 @@
+namespace Simple_Dotnet_App.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Simple-Dotnet-App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Simple_Dotnet_App
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
